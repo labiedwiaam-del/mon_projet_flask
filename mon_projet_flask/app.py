b53d769c1cfba84acfea29fbe048baf3372f6434
labiedwiaam-del <labiedwiaam@gmail.com>
2026-04-28 11:46:22 +0100

    Add games route to Flask application

--- mon_projet_flask/app.py
+++ mon_projet_flask/app.py
@@ -6,5 +6,8 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+@app.route('/games')
+def games():
+    return render_template('games.html')
 if __name__ == '__main__':
     app.run(debug=True)
